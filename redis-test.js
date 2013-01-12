f5dbddc37c2da66fa00e3e485b11e7cef15a3264
Yosuke Torii <jinjorweb@gmail.com>
2013-01-13 04:47:19 +0900

    redis-test更新

--- redis-test.js
+++ redis-test.js
@@ -1,21 +1,18 @@
 var redis = (function(){
-  if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]); 
-    return redis;
-  } else {
-    return require("redis").createClient();
-  }
+  var rtg   = require("url").parse(process.env.NODE_APP_DB_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]); 
+  return redis;
 })();
 
-redis.set('hoge', 'FUGA', function(e){
+redis.set('hoga', 'MOGO', function(e){
   e && console.log(e);
-});
-redis.get('hoge', function(e, data){
-  if(e){
-    console.log(e);
-  }else{
-    console.log(data);
-  }
+  
+  redis.get('hoga', function(e, data){
+    if(e){
+      console.log(e);
+    }else{
+      console.log(data);
+    }
+  });
 });
